ec14bfd793205f56b5d193d9ccb05c3c4297d9b0
Danny Textores <textores.danny@gmail.com>
2020-07-07 17:15:24 +0200

    add component that displays elements in a 4-column grid (lesson 4)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {CardList} from "./components/cardlist/card-list.component";
 import './App.css';
 
 class App extends Component {
@@ -19,11 +20,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => {
-            return <h1 key={monster.id}>{monster.name}</h1>
-          })
-        }
+        <CardList name='test'>
+          {
+            this.state.monsters.map(monster => {
+              return <h1 key={monster.id}>{monster.name}</h1>
+            })
+          }
+        </CardList>
       </div>
     );
   }
