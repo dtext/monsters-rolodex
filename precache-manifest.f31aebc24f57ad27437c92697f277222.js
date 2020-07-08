ce8f31e300ddd6c1bb1caf79d602de707779b1bd
Danny Textores <textores.danny@gmail.com>
2020-07-08 09:33:08 +0200

    Updates

---
+++ precache-manifest.f31aebc24f57ad27437c92697f277222.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b3b353557286b36797bf46b4f6fb3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f04be9fb3ccb2331856c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9680ee62bee18642f9b",
+    "url": "/static/js/2.9b83ecc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b83ecc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f04be9fb3ccb2331856c",
+    "url": "/static/js/main.982f7b79.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
